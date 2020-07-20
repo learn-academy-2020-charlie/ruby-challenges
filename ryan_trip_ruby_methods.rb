070eb6264743ef250d88d4e010bb1c15f5df35ae
jlmooreiii <jloftonmooreiii@gmail.com>
2020-07-20 16:20:06 -0700

    first few method challenges completed

---
+++ ryan_trip_ruby_methods.rb
@@ -0,0 +1,25 @@
+# def sum_these_numbers (num1, num2)
+#     sum = num1 + num2
+# end
+
+# puts sum_these_numbers 13, 72
+
+# def is_even int
+#     if int % 2 == 0
+#         true
+#     else 
+#         false
+#     end #ends the if tree
+# end #ends the function
+
+# puts is_even 12
+
+# def is_valid_num num
+#     if num >= 1 && num <= 10
+#         'Valid'
+#     else
+#         'Invalid'
+#     end
+# end
+
+# puts is_valid_num 10000000000
