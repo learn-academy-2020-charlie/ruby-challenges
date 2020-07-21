736a1a8ba7d61d9e4953dbdc55d9c592ff07c58f
selamhabteab <selamhabteab@gmail.com>
2020-07-21 01:38:42 -0700

    completed, but I have a question

--- selandJak-rmehtods.rb
+++ selandJak-rmehtods.rb
@@ -20,7 +20,7 @@ end
 # Create a method that takes a number as an argument and prints "Valid" if the number is between 1 and 10 (inclusive) and "Invalid" otherwise.
 
 def valid_num number
-    if 1 < number && number < 10
+    if number>=1 && number <= 10
         "#{number} is valid!"
     else
         "#{number} is invalid!"
@@ -39,28 +39,44 @@ def palindrome string
     end 
 end
 # puts palindrome 'sir'
+# puts palindrome 'madam'
 
-
-# -----------------------------------------------------------------------------
+# -------------------------------------#----------------------------------------#
 
 # You are writing the user registration page for a secure web site. On the registration page, the user has to enter a user ID and a password, which has to adhere to the to following criteria:
 
 # User ID and password cannot be the same.
 
-def user_reg (string1, string2)
-    if string1 == string2
-        puts 'user ID and password cannot be the same'
-# User ID and password have to be at least six characters long.
-    elsif
-        string1.length <=5 || string2.length <=5
-        puts 'User ID and password must be at least 6 character!'
-# Password has to contain at least one of: !#$
-    elsif 
-        string2.include? != "!"
-        puts "password must contain at least one of: !#$"
-    end
+# def user_reg (string1, string2)
+#     if string1 == string2
+#         # puts 'user ID and password cannot be the same'
+#  User ID and password have to be at least six characters long.
+#     elsif
+#         string1.length <=5 || string2.length <=5
+#         # puts 'User ID and password must be at least 6 character!'
+#  Password has to contain at least one of: !#$
+#     elsif 
+#         string2.include? != "!"
+        # puts "password must contain at least one of: !#$"
+    # end
 # User ID cannot contain the following characters: !#$ or spaces
 # Password cannot be the word "password".
-end
+# end
 
-user_reg "bobbby", "samantha"
+# user_reg "bobbby", "samantha"
+
+# Ruby also has a command for user input in the terminal called gets. It is a bit like prompt() in JavaScript. And just like prompt(), the input value from gets needs to be stored in a variable to access later. Ruby has a command called chomp that removes any additional line breaks and white space from the end of a string.
+
+puts "Enter your username:"
+user_name = gets.chomp
+puts "Enter your password:"
+password = gets.chomp
+
+def user_login user, pass
+    if user == pass
+        "Sorry, user ID #{user_name} and password #{password} cannot be the same"
+    else
+        "You're good to go!"
+    end
+end
+puts user_name, password
